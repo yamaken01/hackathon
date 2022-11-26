4354c12391f10d7a98be18bd18aa679b77c783fb
hira <len.gom1217@gmail.com>
2022-11-26 15:34:15 +0900

    1534

--- testk.py
+++ testk.py
@@ -10,41 +10,46 @@ root = tk.Tk()
 # window title
 root.title(u"window")
 
+#canvas = tk.Canvas(bg="black", width=400, height=300)
+#canvas.pack()
+pic = tk.PhotoImage(file="./tes/IMG_6257.png", width=400, height=300)
+#canvas.create_image(0, 0, image=pic, anchor=tk.NW)
+
 # windows size
 root.geometry('400x300')
-
-def inputTxt(txt):
-    f = open('./tes/tes.txt','w')
-    f.write(txt)
-    f.close
 
 def deleteEntry(event):
     entry.delete(0, tk.END)
 
 def getText(event):
-    entryValue = 0
-    entryValue = entry.get()
-    inputTxt(entryValue)
+    saveFile = open('./tes/tes.txt','a')
+    saveFile.write(entry.get())
+    saveFile.close
 
-def main():
-    # label
-    static1 = tk.Label(text="test", foreground='#ff0000', background='#90caf9')
-    # これで本当にいいのか？
-    static1.pack()
+def putPicture(event):
+    canvas = tk.Canvas(bg="black", width=400, height=300)
+    canvas.pack()
+    #pic = tk.PhotoImage(file="./tes/human.png", width=40, height=30)
+    canvas.create_image(0, 0, image=pic, anchor=tk.NW)
 
-    # entry
-    entry = tk.Entry(width=50)
-    entry.insert(tk.END, '挿入する文字列')
-    entry.pack()
+# frontend
+# label
+static1 = tk.Label(text="test", foreground='#ff0000', background='#90caf9')
+# これで本当にいいのか？
+static1.pack()
 
-    # button
-    button1 = tk.Button(text = 'なぐる', width=50)
-    button1.bind('<1>', getText)
-    button1.pack()
+# entry
+entry = tk.Entry(width=50)
+entry.insert(tk.END, '挿入する文字列')
+entry.pack()
 
-    root.mainloop()
+# button
+button1 = tk.Button(text = 'なぐる', width=50)
+button1.bind('<1>', getText)
+button1.bind('<3>', putPicture)
+button1.pack()
+
+root.mainloop()
 
 
-if __name__ == '__main__':
-    main()
 
